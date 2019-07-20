d8fd5de541ca833230fa53f46af80cdfeb5b1463
Pham Hong Thang <thang99x.th@gmai.com>
2019-07-20 14:36:18 +0700

    commit

--- BeeduChat/ViewControl/Chat/View/CreateGroupViewController.swift
+++ BeeduChat/ViewControl/Chat/View/CreateGroupViewController.swift
@@ -10,14 +10,16 @@ import UIKit
 import SnapKit
 
 class CreateGroupViewController: UIViewController {
+    var naviConstraint : NSLayoutConstraint!
     var navigationView = UIView()
     var stackView = UIStackView(axis: .horizontal, distribution: .fill, alignment: .center, spacing: 5, design: nil)
     var btnBack = UIButton()
     var lblTitle = UILabel(text: "Tạo nhóm", textColor: Constant.text.color.black, font: nil)
-    var btnSave = UIButton()
+    var btnSave = UILabel(text: "Lưu", textColor: UIColor.blue, font: nil)
     var searchView = UIView()
+    var ceparateView = UIView()
     var txtSearch = UITextField(background: .clear, corner: 0, border: 0, borderColor: .clear, design: nil)
-    var lblMember = UILabel(text: "3 thành viên", textColor: .black, font: nil)
+    var lblMember = UILabel(text: "Chọn", textColor: .black, font: nil)
     var contenView = UIView()
     var tbvContentView = UITableView()
 
@@ -25,68 +27,108 @@ class CreateGroupViewController: UIViewController {
         super.viewDidLoad()
         setupUI()
         // Do any additional setup after loading the view.
+        tbvContentView.register(CreateGroupCell.self, forCellReuseIdentifier: CreateGroupCell.identify)
+        tbvContentView.tableFooterView = UIView()
+        tbvContentView.tableHeaderView = UIView()
+        tbvContentView.separatorStyle = .singleLine
+        tbvContentView.delegate = self
+        tbvContentView.dataSource = self
     }
 }
 extension CreateGroupViewController{
     func setupUI(){
         UINavBar()
+        addCeparate()
         UISearchBar()
         UIContentView()
     }
     func UINavBar(){
         self.view.backgroundColor = UIColor.white
         self.navigationController?.navigationBar.isHidden = true
+        
         self.view.addSubview(navigationView)
+        //        navigationView.dropShadow(scale : true)
+        //        navigationView.backgroundColor = UIColor.gray.withAlphaComponent(0.2)
         navigationView.snp.makeConstraints { (maker) in
-            maker.top.equalTo(self.view.safeAreaLayoutGuide)
-            maker.centerX.equalToSuperview()
-            maker.width.equalToSuperview().offset(-32)
-            maker.height.equalTo(40)
-        }
-        //        navigationView.backgroundColor = UIColor.blue
-        // add stack view to navigationView
-        navigationView.addSubview(stackView)
-        stackView.snp.makeConstraints { (maker) in
-            maker.center.height.equalToSuperview()
+            maker.top.left.right.equalTo(self.view.safeAreaLayoutGuide)
             maker.width.equalToSuperview()
+            //            maker.height.equalTo(Constant.size.naviHeight)
         }
-        //add button menu to stackView
-        stackView.addArrangedSubview(self.btnBack)
-        btnBack.snp.makeConstraints { (maker) in
-            maker.height.equalTo(btnBack.snp.width)
-            maker.height.equalToSuperview().multipliedBy(0.8)
+        naviConstraint = navigationView.heightAnchor.constraint(equalToConstant: Constant.size.naviHeight)
+        naviConstraint.isActive = true
+        /* Navigation
+         **************************************
+         * Menu         Class       noti more *
+         **************************************
+         */
+        navigationView.addSubview(UIStackView(axis: .horizontal, distribution: .fill, alignment: .center, spacing: 4, design: nil)) { (stackNavi) -> (Void) in
+            stackNavi.snp.makeConstraints({ (maker) in
+                maker.center.equalToSuperview()
+                maker.height.equalToSuperview().offset(-Constant.size.padingAround)
+                maker.width.equalToSuperview().offset(-Constant.size.padingAround)
+            })
+            //Btn menu
+            (stackNavi as! UIStackView).addArrangedSubview(self.btnBack)
+            self.btnBack.snp.makeConstraints({ (maker) in
+                maker.width.equalTo(self.btnBack.snp.height)
+                maker.height.equalTo(Constant.size.iconNormal)
+            })
+            self.btnBack.setImage(UIImage(named: "ic_back")?.withRenderingMode(.alwaysTemplate), for: .normal)
+            self.btnBack.tintColor = Constant.color.iconColor
+            self.btnBack.contentMode = .scaleAspectFit
+            //            self.btnMenu.backgroundColor = UIColor.blue
+            
+            //Ten lop
+            (stackNavi as! UIStackView).addArrangedSubview(UIView())
+            
+            self.navigationView.addSubview(UIStackView(axis: .vertical, distribution: .fillEqually, alignment: .center, spacing: 0, design: nil), design: { (stackClass) -> (Void) in
+                stackClass.snp.makeConstraints({ (maker) in
+                    maker.center.equalToSuperview()
+                    //                    maker.height.equalToSuperview()
+                    
+                })
+                (stackClass as! UIStackView).addArrangedSubview(self.lblTitle)
+                self.lblTitle.font = UIFont(name:"HelveticaNeue-Bold", size: 16.0)
+            })
+            //Btn noti
+            (stackNavi as! UIStackView).addArrangedSubview(self.btnSave)
+            self.btnSave.snp.makeConstraints({ (maker) in
+                maker.width.equalTo(self.btnSave.snp.height).multipliedBy(0.8)
+                maker.height.equalTo(Constant.size.iconNormal)
+            })
+            
         }
-        self.btnBack.setImage(UIImage(named: "ic_back")?.withRenderingMode(.alwaysTemplate), for: .normal)
-        self.btnBack.contentMode = .scaleAspectFit
-        stackView.addArrangedSubview(lblTitle)
-        lblTitle.snp.makeConstraints { (maker) in
-            maker.height.equalToSuperview().multipliedBy(1)
+    }
+    func addCeparate(){
+        self.view.addSubview(ceparateView)
+        ceparateView.snp.makeConstraints { (maker) in
+            maker.height.equalTo(1)
+            maker.width.equalToSuperview()
+            maker.top.equalTo(navigationView.snp.bottom)
+            maker.centerX.equalToSuperview()
         }
-        stackView.addArrangedSubview(btnSave)
-        btnSave.snp.makeConstraints { (maker) in
-            maker.height.equalTo(btnSave.snp.width)
-            maker.height.equalToSuperview().multipliedBy(0.8)
-        }
+        ceparateView.backgroundColor = UIColorFromRGB(rgbValue: 0x363636)
     }
     func UISearchBar(){
         self.view.addSubview(searchView)
         searchView.snp.makeConstraints { (maker) in
-            maker.height.equalTo(navigationView.snp.height)
-            maker.width.equalTo(navigationView.snp.width)
-            maker.top.equalTo(navigationView.snp.bottom).offset(16)
+            maker.height.equalTo(navigationView.snp.height).offset(-24)
+            maker.width.equalTo(navigationView.snp.width).offset(-32)
+            maker.top.equalTo(ceparateView.snp.bottom).offset(16)
             maker.centerX.equalTo(navigationView.snp.centerX)
         }
-        searchView.addSubview(UIView(background: .clear, corner: Constant.size.avatarNormal / 2, border: 0, borderColor: UIColor.gray, design: nil)) { (srcView) -> (Void) in
+        searchView.addSubview(UIView(background: UIColorFromRGB(rgbValue: 0xf3f3f3), corner: Constant.size.avatarNormal / 2, border: 0, borderColor: Constant.color.separate, design: nil)) { (srcView) -> (Void) in
             srcView.snp.makeConstraints({ (maker) in
                 maker.height.equalToSuperview()
                 maker.width.equalToSuperview()
                 maker.center.equalToSuperview()
             })
+            //add text field
             srcView.addSubview(self.txtSearch)
             self.txtSearch.snp.makeConstraints({ (maker) in
                 maker.height.equalToSuperview().multipliedBy(0.7)
                 maker.centerY.equalToSuperview()
-                maker.left.equalTo(self.txtSearch.snp.right).offset(8)
+                maker.left.equalToSuperview().offset(8)
                 maker.right.equalToSuperview().offset(-8)
             })
             self.txtSearch.placeholder = "Nhập tên nhóm"
@@ -95,22 +137,59 @@ extension CreateGroupViewController{
     func UIContentView(){
         self.view.addSubview(contenView)
         contenView.snp.makeConstraints { (maker) in
-            maker.height.equalToSuperview()
-            maker.width.equalToSuperview()
-            maker.top.equalTo(searchView.snp.bottom)
-            maker.centerX.equalToSuperview()
+            maker.width.equalTo(searchView.snp.width)
+            maker.top.equalTo(searchView.snp.bottom).offset(12)
+            maker.bottom.equalToSuperview()
+            maker.centerX.equalTo(searchView.snp.centerX)
         }
+        //        chatView.backgroundColor = UIColor.red
         contenView.addSubview(lblMember)
         lblMember.snp.makeConstraints { (maker) in
             maker.top.left.equalToSuperview()
         }
+        lblMember.text = "Chọn"
+        lblMember.font = UIFont(name:"HelveticaNeue-Bold", size: 16)
         contenView.addSubview(tbvContentView)
         tbvContentView.snp.makeConstraints { (maker) in
-            maker.top.equalTo(lblMember.snp.bottom)
-            maker.left.equalToSuperview()
-            maker.height.equalTo(48)
             maker.width.equalToSuperview()
+            maker.centerX.equalToSuperview()
+            maker.top.equalTo(lblMember.snp.bottom).offset(8)
+            maker.height.equalToSuperview()
         }
-        tbvContentView.backgroundColor = UIColor.green
     }
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
+extension CreateGroupViewController : UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return Constant.size.rowHeightChat
+    }
+}
+extension CreateGroupViewController : UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CreateGroupCell.identify, for: indexPath) as! CreateGroupCell
+        cell.selectionStyle = .none
+        if (indexPath.row % 2 == 0) {
+            cell.data = CreateGroupCellModel(avatar: UIImage(named: "ic_ava"), name: "Bố Tú Anh", content: "Phụ huynh")
+        }
+        else{
+            cell.data = CreateGroupCellModel(avatar:UIImage(named: "ic_ava"), name: "Bố Tú Anh", content: "Phụ huynh")
+        }
+        return cell
+    }
+    
+    
 }
