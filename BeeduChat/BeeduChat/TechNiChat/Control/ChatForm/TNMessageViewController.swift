97973ae96a483e33405da94070079bb2480a8651
Pham Hong Thang <thang99x.th@gmai.com>
2019-08-02 11:11:17 +0700

    commit

--- BeeduChat/BeeduChat/TechNiChat/Control/ChatForm/TNMessageViewController.swift
+++ BeeduChat/BeeduChat/TechNiChat/Control/ChatForm/TNMessageViewController.swift
@@ -24,7 +24,7 @@ class TNMessageViewController: TNBaseViewController {
     var txtMess = UITextField()
     var btnAttach = UIButton()
     var btnCalender = UIButton()
-    var btnSend = UIButton(background: Constant.color.btnContinue, corner: 0, border: 0, borderColor: Constant.color.separate, design: nil)
+    var btnSend = UIButton(background: Constant.color.btnContinue, corner: 18, border: 0, borderColor: Constant.color.separate, design: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,13 +60,15 @@ extension TNMessageViewController{
         self.view.addSubview(contactView)
         contactView.snp.makeConstraints { (maker) in
             maker.height.equalTo(navigation.snp.height).offset(-24)
-            maker.width.equalTo(navigation.snp.width).offset(-32)
+            maker.width.equalTo(navigation.snp.width).offset(-2 * Constant.size.paddingView)
             maker.top.equalTo(navigation.snp.bottom).offset(16)
             maker.centerX.equalTo(navigation.snp.centerX)
+            maker.right.equalToSuperview()
         }
         contactView.addSubview(lblSend)
         lblSend.snp.makeConstraints { (maker) in
-            maker.left.top.equalToSuperview().offset(Constant.size.paddingView)
+            maker.left.equalToSuperview()
+            maker.top.equalToSuperview().offset(12)
         }
         contactView.addSubview(scrollView)
         scrollView.snp.makeConstraints { (maker) in
@@ -91,7 +93,7 @@ extension TNMessageViewController{
             maker.centerY.equalTo(lblSend.snp.centerY)
         }
         btnAdd.setImage(UIImage(named: "Add HS")?.withRenderingMode(.alwaysTemplate), for: .normal)
-        btnAdd.tintColor = Constant.color.btnTitle
+        btnAdd.tintColor = Constant.color.iconColor
     }
     func UIContent(){
         self.view.addSubview(ContentView)
@@ -104,26 +106,39 @@ extension TNMessageViewController{
         }
         ContentView.addSubview(btnAttach)
         btnAttach.snp.makeConstraints { (maker) in
-            maker.height.equalTo(Constant.size.btnIcon)
+            maker.height.equalTo(Constant.size.avatarSmall)
             maker.width.equalTo(btnAttach.snp.height)
-            maker.left.equalToSuperview().offset(8)
-            maker.bottom.equalToSuperview().offset(-8)
+            maker.left.equalToSuperview().offset(12)
+            maker.bottom.equalToSuperview().offset(-12)
         }
+        btnAttach.setImage(UIImage(named: "Post Attach")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        btnAttach.tintColor = Constant.color.iconColor
         ContentView.addSubview(btnCalender)
         btnCalender.snp.makeConstraints { (maker) in
-            maker.height.equalTo(Constant.size.btnIcon)
+            maker.height.equalTo(Constant.size.avatarSmall)
             maker.width.equalTo(btnCalender.snp.height)
-            maker.left.equalTo(btnAttach.snp.right).offset(4)
-            maker.bottom.equalToSuperview().offset(-8)
+            maker.left.equalTo(btnAttach.snp.right).offset(8)
+            maker.bottom.equalToSuperview().offset(-12)
         }
+        btnCalender.setImage(UIImage(named: "calendar")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        btnCalender.tintColor = Constant.color.iconColor
         ContentView.addSubview(btnSend)
         btnSend.snp.makeConstraints { (maker) in
-            maker.height.equalTo(Constant.size.btnIcon)
+            maker.height.equalTo(Constant.size.avatarNormal)
             maker.width.equalTo(80)
-            maker.right.equalToSuperview().offset(-8)
-            maker.bottom.equalToSuperview().offset(-8)
+            maker.right.equalToSuperview().offset(-12)
+            maker.bottom.equalToSuperview().offset(-12)
         }
         btnSend.setTitle("Gửi", for: .normal)
         btnSend.clipsToBounds = true
+        ContentView.addSubview(txtMess)
+        txtMess.snp.makeConstraints { (maker) in
+            maker.left.equalTo(btnAttach.snp.left)
+            maker.top.equalToSuperview().offset(8)
+            maker.right.equalTo(btnSend.snp.right)
+            maker.width.equalToSuperview()
+//            maker.height.equalToSuperview()
+        }
+        txtMess.placeholder = "Tuần sau thi học kỳ"
     }
 }
